075e4a65df7c5673f900acdb48c8b32f1486b4f9
agusafriyanto14 <agusafriyanto12345@gmail.com>
2019-03-26 22:37:54 +0700

    pertemuan4

---
+++ app/src/main/java/agusafriyanto/aa/gmail/belajarlayout/MainActivity.java
@@ -0,0 +1,13 @@
+package agusafriyanto.aa.gmail.belajarlayout;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
